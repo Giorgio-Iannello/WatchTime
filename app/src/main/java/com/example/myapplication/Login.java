b1d0ba6c22e678bff6244e881f7a9cdb64f54796
Giorgio-Iannello <giorgio1296@hotmail.com>
2022-05-26 19:58:19 +0200

    Login, registrazione, checkbox che mostra password

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
-
+        EditText usernameET, passwordET;
+        String username, password;
+        CheckBox spuntaPw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        username = "gruppo7";
+        password = "gruppo7";
+        usernameET = (EditText) findViewById(R.id.usernameET);
+        passwordET = (EditText) findViewById(R.id.passwordET);
+        spuntaPw = (CheckBox) findViewById(R.id.spuntaPw);
+
     }
 
     public void clickReg(View view) {
@@ -19,7 +31,33 @@ public class Login extends AppCompatActivity {
         Intent reg = new Intent(Login.this, Registrazione.class);
 
         startActivity(reg);
-        //prova modificadsfdsds
 
+
+    }
+
+    public void cLickAccedi(View view) {
+
+        if(!usernameET.getText().toString().equals(username) || !passwordET.getText().toString().equals(password)){
+            Toast toast = Toast.makeText(this, "Non sei registrato!", Toast. LENGTH_LONG);
+            toast.show();
+
+
+        }
+        else{
+
+        }
+
+    }
+
+    public void clickMP(View view) {
+
+        if (spuntaPw.isChecked()){
+            passwordET.setInputType( InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        }
+        else{
+            passwordET.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        }
+        passwordET.setSelection(passwordET.getText().length());
     }
 }
