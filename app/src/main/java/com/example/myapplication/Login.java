3e8c7bd3efb3298ad233723af32587cc775d22f0
Giorgio-Iannello <giorgio1296@hotmail.com>
2022-05-26 18:40:56 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+
+    public void clickReg(View view) {
+
+        Intent reg = new Intent(Login.this, Registrazione.class);
+
+        startActivity(reg);
+
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Registrazione.java
@@ -0,0 +1,16 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Registrazione extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrazione);
+    }
+}
