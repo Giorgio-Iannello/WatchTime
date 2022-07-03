f2ba30c61c4c896058dd5df6ab86066efe5aa631
Mario Peluso <mariopeluso00@gmail.com>
2022-07-03 23:53:38 +0200

    added details fragment and work in progress on home

---
+++ app/src/main/java/com/example/myapplication/DetailsFragment.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.application.R;
+
+public class DetailsFragment extends Fragment {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+}
