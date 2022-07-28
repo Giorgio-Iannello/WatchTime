d782a345504bd658cba4ef8837b978ac7632626f
Mario Peluso <mariopeluso00@gmail.com>
2022-07-28 23:46:56 +0200

    completed details fragment

---
+++ app/src/main/java/com/example/controller/CommentiDetailsAdapter.java
@@ -0,0 +1,59 @@
+package com.example.controller;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.application.R;
+
+import java.util.ArrayList;
+
+public class CommentiDetailsAdapter extends RecyclerView.Adapter<CommentiDetailsAdapter.ViewHolder>
+{
+    private ArrayList<String> vettCommenti;
+    public static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        private final TextView textView;
+        public ViewHolder(View view)
+        {
+            super(view);
+            textView=view.findViewById(R.id.textViewCommentoItem);
+        }
+        public TextView getTextView() {
+            return textView;
+        }
+    }
+
+    public CommentiDetailsAdapter(ArrayList<String> vettCommenti)
+    {
+        this.vettCommenti=vettCommenti;
+    }
+    public void changeVett(ArrayList<String> vettCommenti)
+    {
+        this.vettCommenti=vettCommenti;
+        notifyAll();
+    }
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType)
+    {
+        View view = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.item_commento, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position)
+    {
+        viewHolder.getTextView().setText(vettCommenti.get(position));
+    }
+
+    @Override
+    public int getItemCount()
+    {
+        return vettCommenti.size();
+    }
+}
--- app/src/main/java/com/example/myapplication/DetailsFragment.java
+++ app/src/main/java/com/example/myapplication/DetailsFragment.java
@@ -2,13 +2,24 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.application.R;
+import com.example.controller.CommentiDetailsAdapter;
+
+import java.util.ArrayList;
 
 public class DetailsFragment extends Fragment {
 
@@ -18,11 +29,48 @@ public class DetailsFragment extends Fragment {
 
         }
     }
-
+    private RecyclerView recyclerView;
+    private CommentiDetailsAdapter adapter;
+    private EditText inputCommento;
+    private Button btnInvio;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
+                             Bundle savedInstanceState)
+    {
         return inflater.inflate(R.layout.fragment_details, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView=view.findViewById(R.id.listCommenti);
+        inputCommento=view.findViewById(R.id.editTextComment);
+        btnInvio=view.findViewById(R.id.buttonSend);
+        ArrayList<String> vettCommenti=new ArrayList<>();
+        vettCommenti.add("Fighissimo madonna");
+        vettCommenti.add("Fa cagare....");
+        adapter=new CommentiDetailsAdapter(vettCommenti);
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                layoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setAdapter(adapter);
+
+        btnInvio.setOnClickListener(v -> {
+            String text=inputCommento.getText().toString().trim();
+            if(text.length()>0)
+            {
+                inputCommento.setText("");
+                vettCommenti.add(text);
+                adapter.changeVett(vettCommenti);
+            }
+            else
+            {
+                Toast.makeText(requireContext(),"AAAAA", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
